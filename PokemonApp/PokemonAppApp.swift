c77951f0885cdfe6f740c0df7b6fbaa138738288
Mert Urhan <mert-urhan@hotmail.com>
2023-03-27 01:17:03 +0300

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Mert Urhan on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
